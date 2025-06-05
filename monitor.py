669130845532e0d68ac39686e078e880a3f9993c
YourName <you@example.com>
2025-06-05 14:30:18 +0200

    Modify gitignore

--- monitor.py
+++ monitor.py
@@ -14,25 +14,43 @@ API_TOKEN = os.getenv('API_TOKEN')
 API_URL = os.getenv('API_URL')
 
 #Configuration pour les logs
-now = datetime.datetime.now().strftime('%Y%m%d')
-log_filename = f'log_{now}.log'
+now = datetime.datetime.now().strftime('%Y-%m-%d')
+log_filename = f'logs/{now}.log'
+json_filename = f'reports/{now}-App.json'
 
 logging.basicConfig(
     level=logging.INFO,
     format='[%(asctime)s] %(levelname)s - %(message)s',
-    filename=log_filename,
     handlers=[
-        logging.FileHandler('logs/script.log'),]
+        logging.FileHandler(log_filename),
+    ]
 )
 
-url = API_URL+API_TOKEN
+url = f'{API_URL}/status?{API_TOKEN}'
 headers = {'Authorization': f'Bearer {API_TOKEN}'}
 
 try:
     if not API_TOKEN:
         raise ValueError('Token vide')
-    r = requests.get(url, headers=headers)
+    
+    r = requests.get(url, headers=headers, timeout = 5)
     logging.info(f'HTTP response from {hide(r.url, API_TOKEN)} with Status: {r.status_code}')
+
+    #Récupérer les informations
+    result = []
+    json = r.json()
+
+    result.append(
+    {
+        'app': json['app'],
+        'status': json['status'],
+        'response_time': json['response_time'],
+        'timestamp': json['timestamp'],
+    })
+
+    with open(json_filename, 'w') as file:
+        file.write('result\n')
+
 
 except requests.exceptions.HTTPError as e:
     logging.error(f'HTTP error: {hide(str(e), API_TOKEN)}')
