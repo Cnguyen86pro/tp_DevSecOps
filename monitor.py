9fc5be76b67056b6396946ab1281ab51003d0775
YourName <you@example.com>
2025-06-05 12:09:59 +0200

    Modification monitor.py

--- monitor.py
+++ monitor.py
@@ -9,24 +9,26 @@ import time
 
 #Chargements des variables d'environnement
 dotenv.load_dotenv()
-
 API_TOKEN = os.getenv('API_TOKEN')
 API_URL = os.getenv('API_URL')
+
+#Configuration pour les logs
+now = datetime.datetime.now().strftime('%Y%m%d')
+log_filename = f'log_{now}.log'
+
+logging.basicConfig(
+    level=logging.INFO,
+    format='[%(asctime)s] %(levelname)s - %(message)s',
+    filename=log_filename
+)
 
 url = API_URL+API_TOKEN
 headers = {'Authorization': f'Bearer {API_TOKEN}'}
 
-# now = datetime.datetime.now().strftime('%Y%m%d')
-# log_filename = f'log_{now}.log'
-
-# logging.basicConfig(
-#     level=logging.INFO,
-#     format='[%(asctime)s] %(levelname)s - %(message)s',
-#     filename=log_filename
-# )
 
 try:
     r = requests.get(url, headers=headers)
+    logging.info(f'HTTP response from {hide(r.url, API_TOKEN)} with Status: {r.status_code}')
 except requests.exceptions.HTTPError as e:
     logging.error(f'HTTP error: {hide(str(e), API_TOKEN)}')
 except requests.exceptions.RequestException as e:
