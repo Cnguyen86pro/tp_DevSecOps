964f3e0118f730d7dc4c4b87ecbdf712a4bbbb84
YourName <you@example.com>
2025-06-05 15:25:05 +0200

    Modify fake_api et monitor

--- fake_api.py
+++ fake_api.py
@@ -9,12 +9,12 @@ import os
 app = FastAPI()
 
 dotenv.load_dotenv()
-API_TOKEN = os.getenv('API_TOKEN')
+api_token = os.getenv('API_TOKEN')
 
 @app.middleware('http')
 async def check_token(request: Request, call_next):
   auth = request.headers.get('Authorization')
-  if not auth or auth != f'Bearer {API_TOKEN}':
+  if not auth or auth != f'Bearer {api_token}':
     return JSONResponse(status_code=401, content = '{"detail": "Unauthorized: Invalid or missing token"}')  
     #raise HTTPException(status_code=401, detail='Unauthorized: Invalid or missing token')
   return await call_next(request)
--- monitor.py
+++ monitor.py
@@ -10,8 +10,8 @@ import time
 
 #Chargements des variables d'environnement
 dotenv.load_dotenv()
-API_TOKEN = os.getenv('API_TOKEN')
-API_URL = os.getenv('API_URL')
+api_token = os.getenv('API_TOKEN')
+api_url = os.getenv('API_URL2')
 
 #Configuration pour les logs
 now = datetime.datetime.now().strftime('%Y-%m-%d')
@@ -26,34 +26,24 @@ logging.basicConfig(
     ]
 )
 
-url = f'{API_URL}status?{API_TOKEN}'
-headers = {'Authorization': f'Bearer {API_TOKEN}'}
+url = f'{api_url}status?{api_token}'
+headers = {'Authorization': f'Bearer {api_token}'}
 
 try:
-    if not API_TOKEN:
+    if not api_token:
         raise ValueError('Token vide')
     
     r = requests.get(url, headers=headers, timeout = 5)
-    logging.info(f'HTTP response from {hide(r.url, API_TOKEN)} with Status: {r.status_code}')
+    logging.info(f'HTTP response from {hide(r.url, api_token)} with Status: {r.status_code}')
 
     #Récupérer les informations
-    result = []
     json = r.json()
-
-    result.append(
-    {
-        'app': json['app'],
-        'status': json['status'],
-        'response_time': json['response_time'],
-        'timestamp': json['timestamp'],
-    })
-
     with open(json_filename, 'w') as file:
-        file.write('result\n')
+        file.write(f'{json}\n')
 
 
 except requests.exceptions.HTTPError as e:
-    logging.error(f'HTTP error: {hide(str(e), API_TOKEN)}')
+    logging.error(f'HTTP error: {hide(str(e), api_token)}')
 
 except requests.exceptions.RequestException as e:
-    logging.error(f'Network error on {hide(url, API_TOKEN)}')
+    logging.error(f'Network error on {hide(url, api_token)}')
